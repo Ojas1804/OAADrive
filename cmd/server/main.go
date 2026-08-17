7e4a77a84fbf6f60e92d6329b3f08bf4d973956f
Ojas1804 <ojas.srivastava.2001@gmail.com>
2026-08-18 00:52:04 +0530

    new

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,22 +16,22 @@ import (
 )
 
 type config struct {
-	Port               string
-	DatabaseURL        string
-	SeaweedfsEndpoint  string
+	Port string
+	DatabaseURL string
+	SeaweedfsEndpoint string
 	SeaweedfsAccessKey string
 	SeaweedfsSecretKey string
-	JWTSecret          string
+	JWTSecret string
 }
 
 func loadConfig() config {
 	return config{
-		Port:               getEnv("PORT", "8080"),
-		DatabaseURL:        getEnv("DATABASE_URL", ""),
-		SeaweedfsEndpoint:  getEnv("SEAWEEDFS_S3_ENDPOINT", ""),
+		Port: getEnv("PORT", "8080"),
+		DatabaseURL: getEnv("DATABASE_URL", ""),
+		SeaweedfsEndpoint: getEnv("SEAWEEDFS_S3_ENDPOINT", ""),
 		SeaweedfsAccessKey: getEnv("SEAWEEDFS_ACCESS_KEY", ""),
 		SeaweedfsSecretKey: getEnv("SEAWEEDFS_SECRET_KEY", ""),
-		JWTSecret:          getEnv("JWT_SECRET", ""),
+		JWTSecret: getEnv("JWT_SECRET", ""),
 	}
 }
 
@@ -52,9 +52,9 @@ func main() {
 		cfg.Port, cfg.DatabaseURL != "", cfg.SeaweedfsEndpoint != "")
 
 	server := &http.Server{
-		Addr:         ":" + cfg.Port,
-		Handler:      mux,
-		ReadTimeout:  10 * time.Second,
+		Addr: ":" + cfg.Port,
+		Handler: mux,
+		ReadTimeout: 10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 
@@ -68,6 +68,6 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(w).Encode(map[string]string{
 		"status": "ok",
-		"time":   time.Now().UTC().Format(time.RFC3339),
+		"time": time.Now().UTC().Format(time.RFC3339),
 	})
 }
